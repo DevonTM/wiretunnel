bef3300fdc7aeaa7b7da9c5cf137832da3c432ac
Irfan Ammar Afif <ipangcheater@gmail.com>
2024-04-16 17:34:04 +0700

    Increase dial timeout in NewResolver() connectivity test

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -39,7 +39,7 @@ func NewResolver(d *wiredialer.WireDialer) (*Resolver, error) {
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	go func() {
